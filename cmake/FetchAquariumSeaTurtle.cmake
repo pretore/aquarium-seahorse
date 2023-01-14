5325f982a80eb32f27bc2278e2d801b951885371
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-01-14 15:49:50 +0000

    - Updated dependency of ``aquarium-triggerfish`` to v1.1.5. - Updated dependency of ``aquarium-sea-turtle`` to v2.2.2. - Updated test dependency ``aquarium-cmocka`` to v1.3.0.

--- cmake/FetchAquariumSeaTurtle.cmake
+++ cmake/FetchAquariumSeaTurtle.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         aquarium-sea-turtle
         GIT_REPOSITORY https://github.com/pretore/aquarium-sea-turtle.git
-        GIT_TAG v2.2.1
+        GIT_TAG v2.2.2
         GIT_SHALLOW 1
 )
 
--- cmake/FetchAquariumTriggerfish.cmake
+++ cmake/FetchAquariumTriggerfish.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         aquarium-triggerfish
         GIT_REPOSITORY https://github.com/pretore/aquarium-triggerfish.git
-        GIT_TAG v1.1.4
+        GIT_TAG v1.1.5
         GIT_SHALLOW 1
 )
 
